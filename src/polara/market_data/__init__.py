5986a3d2a8d029be8296e7a1921a91c915a2d49d
cgncn <main@polaraworld.com>
2026-04-29 09:43:16 +0200

    chore: export intraday strategies and session helpers from __init__

---
+++ src/polara/market_data/__init__.py
@@ -0,0 +1,17 @@
+from polara.market_data.session import (
+    get_prev_session_bars,
+    get_session_bars,
+    is_session_bar,
+    opening_range_end_utc,
+    session_close_utc,
+    session_open_utc,
+)
+
+__all__ = [
+    "get_prev_session_bars",
+    "get_session_bars",
+    "is_session_bar",
+    "opening_range_end_utc",
+    "session_close_utc",
+    "session_open_utc",
+]
--- src/polara/research_engine/strategies/__init__.py
+++ src/polara/research_engine/strategies/__init__.py
@@ -1,13 +1,21 @@
 from polara.research_engine.strategies.bollinger_bands import BollingerBandStrategy
+from polara.research_engine.strategies.gap_fill import GapFillStrategy
 from polara.research_engine.strategies.ma_crossover import MACrossoverStrategy
 from polara.research_engine.strategies.macd import MACDStrategy
 from polara.research_engine.strategies.momentum import MomentumStrategy
+from polara.research_engine.strategies.opening_range_breakout import OpeningRangeBreakoutStrategy
+from polara.research_engine.strategies.prev_day_breakout import PrevDayBreakoutStrategy
 from polara.research_engine.strategies.rsi_mean_reversion import RSIMeanReversionStrategy
+from polara.research_engine.strategies.vwap_mean_reversion import VWAPMeanReversionStrategy
 
 __all__ = [
     "BollingerBandStrategy",
+    "GapFillStrategy",
     "MACrossoverStrategy",
     "MACDStrategy",
     "MomentumStrategy",
+    "OpeningRangeBreakoutStrategy",
+    "PrevDayBreakoutStrategy",
     "RSIMeanReversionStrategy",
+    "VWAPMeanReversionStrategy",
 ]
